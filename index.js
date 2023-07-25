3477bf27b6b39bbf5faafa1922b12296c61843b8
arifkhan676 <arifulhasan5500@gmail.com>
2023-07-25 20:11:11 +0600

    Product has been added

--- index.js
+++ index.js
@@ -9,36 +9,81 @@ const product = [
     {
         id: 2,
         name: "Lichi",
-        price: 100,
-        comment: "good"
+        price: 200,
+        comment: "Best"
     },
     {
         id: 3,
         name: "Banans",
-        price: 100,
-        comment: "good"
+        price: 50,
+        comment: "average"
+    },
+    {
+        id: 4,
+        name: "Papaya",
+        price: 150,
+        comment: "Delicious"
     }
 
 ]
 
-const productList = product.map((item) => {
+const productList = [...new Set(product.map((item) => {
     return (item);
 })
+)]
 
-document.getElementById('x').innerHTML = productList.map((item) => {
+document.getElementById('x').innerHTML = productList.map((item, idx) => {
     var { name, price, comment } = item;
     return (
         `<div class="card" >
-  <div class="card-body">
-     <h3> ${item.name} </h3>
-     <h2> ${item.price} </h2>
-     <p> ${item.comment} </p>
-     <button class='btn btn-primary' onclick = 'addCart()'  > Add to cart </button>
-
-  </div>
- </div>`
+        <div class="card-body">
+           <h3> ${name} </h3>
+           <h2> ${price} </h2>
+           <p> ${comment} </p>
+           <button class='btn btn-primary' onclick = 'addCart(${idx})'  > Add to cart </button>
+      
+        </div>
+       </div>`
     )
-})
+}).join('')
+
+
+
+const cart = [];
+
+function addCart(a) {
+    cart.push({ ...productList[a] });
+    display();
+}
+function remove(a) {
+    cart.pop();
+    display();
+}
+
+
+function display(a) {
+    if (cart.length == 0) {
+        document.getElementById('y').innerHTML = 'You cart is empty'
+
+    } else {
+        document.getElementById('y').innerHTML = cart.map((items) => {
+            var { name, price, comment } = items;
+            return (
+                `<div class="card" >
+                <div class="card-body">
+                   <h3> ${name} </h3>
+                   <h2> ${price} </h2>
+                   <p> ${comment} </p>
+                   <button class='btn btn-primary' onclick = 'remove()'  > Remove </button>
+              
+                </div>
+               </div>`
+            )
+        }).join('')
+    }
+
+
+}
 
 
 /*
